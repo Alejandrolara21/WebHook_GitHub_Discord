868ea2db711454dbc3afbaf5fd9253cec3986731
Alejandrolara21 <alejandroguacanemelara@gmail.com>
2024-05-10 20:12:29 -0500

    Implementing validate token in webHook

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from 'express';
 import { envs } from './config';
 import { GitHubController } from './presentation/github/controller.github';
+import { ValidateTokenGitHubMiddleware } from './presentation/middlewares/validateTokenGitHub.middleware';
 
 (() => {
     main();
@@ -13,6 +14,8 @@ function main () {
     const controller = new GitHubController();
 
     app.use(express.json());
+
+    app.use(ValidateTokenGitHubMiddleware.verifySignature);
 
     app.post('/api/github', controller.webhookHandler);
 
